40b2213818212ee693dd65acfe0c3ebba12f48c9
Alan11774 <alanblacksky@gmail.com>
2024-10-04 19:02:40 -0600

    main

---
+++ 4Oct/4Oct/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  4Oct
+//
+//  Created by Alan Ulises on 04/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 4Oct/4OctTests/_OctTests.swift
@@ -0,0 +1,17 @@
+//
+//  _OctTests.swift
+//  4OctTests
+//
+//  Created by Alan Ulises on 04/10/24.
+//
+
+import Testing
+@testable import _Oct
+
+struct _OctTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
