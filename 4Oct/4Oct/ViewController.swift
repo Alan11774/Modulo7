b09f6d96ae02a7f7cee1a74a064c52edbef41371
Alan11774 <alanblacksky@gmail.com>
2024-10-04 20:53:06 -0600

    main Modifications to main screen and second screen

--- 4Oct/4Oct/ViewController.swift
+++ 4Oct/4Oct/ViewController.swift
@@ -8,12 +8,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet weak var txtCorreo: UITextField!
+    
+    @IBOutlet weak var txtContraseña: UITextField!
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         // Do any additional setup after loading the view.
+        let ud = UserDefaults.standard
+        if ud.string(forKey: "login") == "OK"{
+            performSegue(withIdentifier: "loginOk", sender: self)
+        }
+        
+        
     }
-
-
-}
+    /*func validateUserDefaulst(var type: String){
+        let ud = UserDefaults.standard
+        ud.set("OK", forKey: "login")
+        performSegue(withIdentifier: "loginOk", sender: self)
+    }*/
+    // We can track the action of the biutton clicked in this class
+    @IBAction func btnEntrarTouch(_ sender: Any) {
+        var message = ""
+        let correo = txtCorreo.text ?? ""
+        let contraseña = txtContraseña.text ?? ""
+        if correo.isEmpty {
+            message += "Ingrese un correo\n"
+        }else if contraseña.isEmpty {
+            message += "Ingrese una contraseña\n"
+        }
+        else{
+            // With regex we can evaluate the string using the predicate in correo valido.
+            // we use correo valido to evaluate correo variable.
+            let regex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
+            let correoValido = NSPredicate(format: "SELF MATCHES %@", regex)
+            // if not expression
+            if !correoValido.evaluate(with: correo){
+                message = "No es un correo valido"
+            }
+            
+        }
+        if message.isEmpty{
+            print("Haz iniciado sesion")
+            let ud = UserDefaults.standard
+            ud.set("OK", forKey: "login")
+            performSegue(withIdentifier: "loginOk", sender: self)
+            
+        }
+        else{
+            // Like the toast this is the opup messages.
+            //message += "Bienvenido "
+            let ac = UIAlertController(title: "Hola", message: message, preferredStyle: .alert)
+            let action = UIAlertAction(title: "Ok", style: .default)
+                ac.addAction(action)
+                self.present(ac, animated: true)
+            }
+        }
+    }
+    
 
